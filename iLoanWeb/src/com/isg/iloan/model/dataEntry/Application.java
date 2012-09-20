5aac9a584429ff812beb38e4bcfcc3adbe3517a7
gilmarte <gil777marte@gmail.com>
2012-09-20 20:36:09 +0800

    modified

--- iLoanWeb/src/com/isg/iloan/model/dataEntry/Application.java
+++ iLoanWeb/src/com/isg/iloan/model/dataEntry/Application.java
@@ -3,86 +3,102 @@ package com.isg.iloan.model.dataEntry;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
+import org.apache.log4j.Logger;
+import org.hibernate.annotations.ForeignKey;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.engine.CascadeStyle;
+
+import com.isg.iloan.controller.functions.dataEntry.CreditCardDetailsViewCtrl;
 
 /**
  ** Class Application
  **/
 
+//@Table(name="APPLICATION",
+//		uniqueConstraints = @UniqueConstraint(columnNames="CARDTYPECODE,APPSTATUS")
+//	)
+
+
 @Entity
 @Table(name="APPLICATION")
 public class Application implements Serializable{ 
 	
-  
+	private static Logger logger = Logger.getLogger(Application.class);
+	
   private static final long serialVersionUID = 7404651072498175937L;
-//  @Id
-//  @SequenceGenerator(name="SEQ_ID",sequenceName="SEQ_ID",allocationSize=1)
-//  @GeneratedValue(strategy = GenerationType.SEQUENCE)
-  @Id
-  @GenericGenerator(name="generator", strategy="increment")
-  @GeneratedValue(generator="generator")
-  private int id;
-  
-  @Column(name="APPID")
-  @Id
-  @GenericGenerator(name="generator", strategy="increment")
-  @GeneratedValue(generator="generator")
-  private int applicationId;
-  
-  @Column(name="ACCEPTCLASSICCARD")
-  private boolean acceptClassicCard;
-  
-  @Column(name="ACCEPTSAVEANDSWIPE")
-  private boolean acceptSaveAndSwipe;
-  
-  @Column(name="APPDATE")
-  private Date dateOfApplication;
-  
-  @Column(name="CARDTYPECODE")
-  private String cardTypeCode;
-  
-  @Column(name="CARDTYPEDESC")
-  private String cardTypeDesc;
-  
-  @Column(name="APPSTATUS")
-  private int appStatusCode;
-  
-  @Column(name="APPSTATUSDESC")
-  private String appStatusDesc;
  
+//  @Id
+//  @GenericGenerator(name="generator", strategy="increment")
+//  @GeneratedValue(generator="generator")
+  
+  
+  @Id
+  @SequenceGenerator(name="APPLICATION_SEQUENCE",sequenceName="APPLICATION_SEQUENCE",allocationSize=1)
+  @GeneratedValue(generator="APPLICATION_SEQUENCE", strategy = GenerationType.SEQUENCE)  
+  @Column(name="APPID")
+  private int applicationId;
+    
+  @Column(name="ACCEPTCLASSICCARD")
+  private boolean acceptClassicCard;  
+  @Column(name="ACCEPTSAVEANDSWIPE")
+  private boolean acceptSaveAndSwipe;  
+  //@Temporal(TemporalType.TIMESTAMP)
+  @Column(name="APPDATE")
+  private Date dateOfApplication;  
+  @Column(name="CARDTYPECODE")
+  private String cardTypeCode;  
+  @Column(name="CARDTYPEDESC")
+  private String cardTypeDesc;  
+  @Column(name="APPSTATUS")
+  private int appStatusCode;  
+  @Column(name="APPSTATUSDESC")
+  private String appStatusDesc; 
   @Column(name="ENROLLSOS")
   private boolean enrollSOS;
   
   
-	public Application() {
+
+  @Column(name="JOB_DETAIL_ID")
+  private int jobDetailId;
+  
+  
+  @OneToOne(cascade=CascadeType.ALL)
+  @PrimaryKeyJoinColumn(name="APPID", referencedColumnName="JOB_DETAIL_ID")
+  private JobDetail jobDetail;
+  
+  
+  
+  public Application() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
 	 * Use this method for update.
 	 * @param appBean
 	 */
-	public void copyProperties(Application appBean){
-		
-	}
+	public void copyProperties(Application appBean){}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	
 	public int getApplicationId() {
 		return applicationId;
 	}
@@ -153,6 +169,25 @@ public class Application implements Serializable{
 
 	public void setEnrollSOS(boolean enrollSOS) {
 		this.enrollSOS = enrollSOS;
+	}
+	
+	
+
+	public JobDetail getJobDetail() {
+		return jobDetail;
+	}
+
+	public void setJobDetail(JobDetail jobDetail) {
+		this.jobDetail = jobDetail;
+	}
+
+	
+	public int getJobDetailId() {		
+		return jobDetailId;
+	}
+
+	public void setJobDetailId(int jobDetailId) {
+		this.jobDetailId = jobDetailId;
 	}
 
   
--- iLoanWeb/src/com/isg/iloan/model/dataEntry/JobDetail.java
+++ iLoanWeb/src/com/isg/iloan/model/dataEntry/JobDetail.java
@@ -1,31 +1,232 @@
 package com.isg.iloan.model.dataEntry;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  ** Class JobDetail
  **/
-public class JobDetail { 
-  //Fields
-  private int id;
+@Entity
+@Table(name="JOB_DETAIL")
+public class JobDetail implements Serializable { 
+
+ 
+  
+  @Id
+  @SequenceGenerator(name="APPLICATION_SEQUENCE",sequenceName="APPLICATION_SEQUENCE",allocationSize=1)
+  @GeneratedValue(generator="APPLICATION_SEQUENCE", strategy = GenerationType.SEQUENCE)  
+  @Column(name="JOB_DETAIL_ID")
   private int jobDetailId;
-  private int applicationId;
-  private List<Fund> sourceOfFunds;
+  
+  
+  @Column(name="APPID")
+  private int applicationId; 
+  
+  
+  @Column(name="COMPANY")
   private String companyName;
+  @Column(name="OCCUPATION")
   private String occupation;
+  @Column(name="WORKNATURE")
   private String natureOfWork;
-  private Address businessAddress;
+  @Column(name="YEARS_EMPLOYED")
   private int yearsWithCurrentEmployer;
+  @Column(name="TOTAL_YRS_WORKING")
   private int totalWorkingYears;
+  @Column(name="MONTHLYGROSS")
   private long grossMonthlyIncome;
+  @Column(name="SPOUSE_FAM_NAME")
   private String spouseFamilyName;
+  @Column(name="SPOUSE_MIDD_NAME")
   private String spouseMiddleName;
+  @Column(name="SPOUSE_DOB")
   private Date spouseDOB;
 
+  
+//  @OneToOne(fetch=FetchType.LAZY)
+//  @JoinColumn(name="ADDRESS_ID")
+//  private Address businessAddress;
+  
+  //private List<Fund> sourceOfFunds;
+  
+  
+  
   //Constructors
-  JobDetail(){}
+  public JobDetail(){}
 
+
+
+
+public int getJobDetailId() {
+	return jobDetailId;
+}
+
+
+public void setJobDetailId(int jobDetailId) {
+	this.jobDetailId = jobDetailId;
+}
+
+
+//public int getApplicationId() {
+//	return applicationId;
+//}
+//public void setApplicationId(int applicationId) {
+//	this.applicationId = applicationId;
+//}
+
+
+public String getCompanyName() {
+	return companyName;
+}
+
+
+public void setCompanyName(String companyName) {
+	this.companyName = companyName;
+}
+
+
+public String getOccupation() {
+	return occupation;
+}
+
+
+public void setOccupation(String occupation) {
+	this.occupation = occupation;
+}
+
+
+public String getNatureOfWork() {
+	return natureOfWork;
+}
+
+
+public void setNatureOfWork(String natureOfWork) {
+	this.natureOfWork = natureOfWork;
+}
+
+
+public int getYearsWithCurrentEmployer() {
+	return yearsWithCurrentEmployer;
+}
+
+
+public void setYearsWithCurrentEmployer(int yearsWithCurrentEmployer) {
+	this.yearsWithCurrentEmployer = yearsWithCurrentEmployer;
+}
+
+
+public int getTotalWorkingYears() {
+	return totalWorkingYears;
+}
+
+
+public void setTotalWorkingYears(int totalWorkingYears) {
+	this.totalWorkingYears = totalWorkingYears;
+}
+
+
+public long getGrossMonthlyIncome() {
+	return grossMonthlyIncome;
+}
+
+
+public void setGrossMonthlyIncome(long grossMonthlyIncome) {
+	this.grossMonthlyIncome = grossMonthlyIncome;
+}
+
+
+public String getSpouseFamilyName() {
+	return spouseFamilyName;
+}
+
+
+public void setSpouseFamilyName(String spouseFamilyName) {
+	this.spouseFamilyName = spouseFamilyName;
+}
+
+
+public String getSpouseMiddleName() {
+	return spouseMiddleName;
+}
+
+
+public void setSpouseMiddleName(String spouseMiddleName) {
+	this.spouseMiddleName = spouseMiddleName;
+}
+
+
+public Date getSpouseDOB() {
+	return spouseDOB;
+}
+
+
+public void setSpouseDOB(Date spouseDOB) {
+	this.spouseDOB = spouseDOB;
+}
+
+
+
+
+public int getApplicationId() {
+	return applicationId;
+}
+
+
+
+
+public void setApplicationId(int applicationId) {
+	this.applicationId = applicationId;
+}
+
+
+
+
+
+
+
+
+
+
+//public Address getBusinessAddress() {
+//	return businessAddress;
+//}
+//
+//
+//public void setBusinessAddress(Address businessAddress) {
+//	this.businessAddress = businessAddress;
+//}
+
+//
+//public List<Fund> getSourceOfFunds() {
+//	return sourceOfFunds;
+//}
+//public void setSourceOfFunds(List<Fund> sourceOfFunds) {
+//	this.sourceOfFunds = sourceOfFunds;
+//}
+
+  
+  
+  
+  
+  
+  
+  
   //Methods
 
   //Inner classes
