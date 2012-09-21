46f802b128152ff70414975aae4c1f1ff361f147
gilmarte <gil777marte@gmail.com>
2012-09-21 19:55:46 +0800

    modified Fund class added @Entity annotation

--- iLoanWeb/src/com/isg/iloan/model/dataEntry/Fund.java
+++ iLoanWeb/src/com/isg/iloan/model/dataEntry/Fund.java
@@ -3,16 +3,17 @@ package com.isg.iloan.model.dataEntry;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-/**
- ** Class Fund
- **/
+@Entity
+@Table(name="FUND")
 public class Fund implements Serializable{ 
   
   private static final long serialVersionUID = -7124272410437844513L;
