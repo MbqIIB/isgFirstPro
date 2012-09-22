84e3d6b6220217defab8d91a51c6949beb33a012
gilmarte <gil777marte@gmail.com>
2012-09-22 13:30:50 +0800

    added ID constant

---
+++ iLoanWeb/src/com/isg/iloan/commons/IDs.java
@@ -0,0 +1,42 @@
+package com.isg.iloan.commons;
+
+public class IDs {
+
+	public static final String CLASSIC_MASTERCARD = "classicMasterCard";
+	public static final String CLASSIC_VISACARD = "classicVisaCard";
+	public static final String GOLD_MASTERCARD = "goldMasterCard";
+	public static final String GOLD_VISACARD = "goldMasterCard";
+	public static final String MLITE_MASTERCARD = "mLiteMasterCard";
+	public static final String FEMME_VISACARD = "femmeVisaCard";
+	public static final String ACCEPT_CLASSIC_CARD = "acceptClassicCard";
+	public static final String NOT_ACCEPT_CLASSIC_CARD = "notAcceptClassicCard";
+	
+	
+	public static final String ACCEPT_SAVE_SWIPE = "acceptSaveAndSwipe";
+	public static final String NOT_ACCEPT_SAVE_SWIPE = "notAcceptSaveAndSwipe";
+	public static final String EXISTING_CC1 = "existingCC1";
+	public static final String EXISTING_CC2 = "existingCC2";
+	public static final String EXISTING_CC3 = "existingCC3";
+	public static final String ECC1_LIMIT = "ecc1Limit";
+	public static final String ECC2_LIMIT = "ecc2Limit";
+	public static final String ECC3_LIMIT = "ecc3Limit";
+	public static final String ECC1_DOM = "ecc1DOM";
+	public static final String ECC2_DOM = "ecc2DOM";
+	public static final String ECC3_DOM = "ecc3DOM";
+	
+	public static final String OCC1 = "occ1";
+	public static final String OCC2 = "occ2";
+	public static final String OCC3 = "occ3";
+	public static final String OCC1_NUM = "occ1Num";
+	public static final String OCC2_NUM = "occ2Num";
+	public static final String OCC3_NUM = "occ3Num";
+	public static final String OCC1_LIMIT = "occ1Limit";
+	public static final String OCC2_LIMIT = "occ2Limit";
+	public static final String OCC3_LIMIT = "occ3Limit";
+	public static final String OCC1_DOM = "occ1DOM";
+	public static final String OCC2_DOM = "occ2DOM";
+	public static final String OCC3_DOM = "occ3DOM";
+	
+	
+	
+}
