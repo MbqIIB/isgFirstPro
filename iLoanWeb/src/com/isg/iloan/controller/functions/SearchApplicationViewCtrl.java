2a60c1f048611f7d621f220931576ccadba85abb
sheena <purplelet23@yahoo.com>
2012-10-01 10:19:53 +0800

    updated search

--- iLoanWeb/src/com/isg/iloan/controller/functions/SearchApplicationViewCtrl.java
+++ iLoanWeb/src/com/isg/iloan/controller/functions/SearchApplicationViewCtrl.java
@@ -5,7 +5,13 @@ package com.isg.iloan.controller.functions;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zkplus.spring.SpringUtil;
 import org.zkoss.zul.Button;
+
+import com.isg.iloan.service.ApplicationService;
+import com.isg.iloan.service.ApplicationServiceImpl;
+import com.isg.iloan.service.ILOVService;
+import com.isg.iloan.service.LOVServiceImpl;
 
 /**
  * @author sheena.catacutan
@@ -22,15 +28,21 @@ public class SearchApplicationViewCtrl extends GenericForwardComposer {
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
-		// TODO Auto-generated method stub
-
 	}
+	
+	public ILOVService getLOVService(){
+	   return  (ILOVService)SpringUtil.getBean("lovService", LOVServiceImpl.class);
+	}
+	
+	
 	//onCheck$acceptSaveAndSwipe()
 	public void onClick$searchAppButton(){
 		alert("button is clicked.");
+		System.out.println( getLOVService().retrieveById(1).getDesc());
 	}
 	
 	public void onClick$clearBtn(){
 		
 	}
+	
 }
--- iLoanWeb/src/com/isg/iloan/controller/viewModel/SearchApplicationViewModel.java
+++ iLoanWeb/src/com/isg/iloan/controller/viewModel/SearchApplicationViewModel.java
@@ -1,10 +1,12 @@
 package com.isg.iloan.controller.viewModel;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
-import com.isg.iloan.model.dataEntry.PersonalData;
+import org.zkoss.zkplus.spring.SpringUtil;
+
+import com.isg.iloan.service.ILOVService;
+import com.isg.iloan.service.LOVServiceImpl;
 import com.isg.iloan.service.SearchCriteria;
 
 public class SearchApplicationViewModel {
@@ -13,46 +15,57 @@ public class SearchApplicationViewModel {
 	private static List<String> creditCardType = new ArrayList<String>();
 	private static List<SearchCriteria> applicationDetails = new ArrayList<SearchCriteria>();
 
-	
-	
-	static{
-		appStatus.add("New");
-		appStatus.add("Pending");
-		appStatus.add("Approved");
-		appStatus.add("Rejected");
-		
-		creditCardType.add("M Lite Mastercard");
-		creditCardType.add("Femme Visa");
-		creditCardType.add("Classic Mastercard");
-		creditCardType.add("Classic Visa");
-		creditCardType.add("Gold Mastercard");
-		creditCardType.add("Gold Visa");
-		
-//		PersonalData pd1 = new PersonalData("Geronimo", "Zara", "Reyes");
-//		ApplicationBean app1 = new ApplicationBean(Calendar.getInstance().getTime(), "Femme Visa", "Approved");
-//		applicationDetails.add(new SearchCriteria(app1, pd1));
-//		PersonalData pd2 = new PersonalData("Ferragamo", "Sylverter", "Santos");
-//		ApplicationBean app2 = new ApplicationBean(Calendar.getInstance().getTime(), "M Lite Mastercard", "Rejected");
-//		applicationDetails.add(new SearchCriteria(app2, pd2));
-//		PersonalData pd3 = new PersonalData("Khors", "Michael", "Tan");
-//		ApplicationBean app3 = new ApplicationBean(Calendar.getInstance().getTime(), "Gold Visa", "New");
-//		applicationDetails.add(new SearchCriteria(app3, pd3));
-//		PersonalData pd4 = new PersonalData("York", "Donna Karan", "New");
-//		ApplicationBean app4 = new ApplicationBean(Calendar.getInstance().getTime(), "Classic Mastercard", "Pending");
-//		applicationDetails.add(new SearchCriteria(app4, pd4));
-		
+	public static ILOVService getLOVService(){
+		 return  (ILOVService)SpringUtil.getBean("lovService", LOVServiceImpl.class);
 	}
 	
+	static {
+		appStatus.add(getLOVService().retrieveById(1).getDesc());
+		appStatus.add(getLOVService().retrieveById(2).getDesc());
+		appStatus.add(getLOVService().retrieveById(3).getDesc());
+		appStatus.add(getLOVService().retrieveById(4).getDesc());
+		
+		creditCardType.add(getLOVService().retrieveById(5).getDesc());
+		creditCardType.add(getLOVService().retrieveById(6).getDesc());
+		creditCardType.add(getLOVService().retrieveById(7).getDesc());
+		creditCardType.add(getLOVService().retrieveById(8).getDesc());
+		creditCardType.add(getLOVService().retrieveById(9).getDesc());
+		creditCardType.add(getLOVService().retrieveById(10).getDesc());
+		
+		// PersonalData pd1 = new PersonalData("Geronimo", "Zara", "Reyes");
+		// ApplicationBean app1 = new
+		// ApplicationBean(Calendar.getInstance().getTime(), "Femme Visa",
+		// "Approved");
+		// applicationDetails.add(new SearchCriteria(app1, pd1));
+		// PersonalData pd2 = new PersonalData("Ferragamo", "Sylverter",
+		// "Santos");
+		// ApplicationBean app2 = new
+		// ApplicationBean(Calendar.getInstance().getTime(),
+		// "M Lite Mastercard", "Rejected");
+		// applicationDetails.add(new SearchCriteria(app2, pd2));
+		// PersonalData pd3 = new PersonalData("Khors", "Michael", "Tan");
+		// ApplicationBean app3 = new
+		// ApplicationBean(Calendar.getInstance().getTime(), "Gold Visa",
+		// "New");
+		// applicationDetails.add(new SearchCriteria(app3, pd3));
+		// PersonalData pd4 = new PersonalData("York", "Donna Karan", "New");
+		// ApplicationBean app4 = new
+		// ApplicationBean(Calendar.getInstance().getTime(),
+		// "Classic Mastercard", "Pending");
+		// applicationDetails.add(new SearchCriteria(app4, pd4));
+
+	}
+
 	public List<String> getAppStatus() {
 		return appStatus;
 	}
-	
-	public List<String> getCreditCardType(){
+
+	public List<String> getCreditCardType() {
 		return creditCardType;
 	}
-	
+
 	public List<SearchCriteria> getApplicationDetails() {
 		return applicationDetails;
 	}
-	
+
 }
